a2281a86db43b8996bf650d879899fb071e30e7c
lloydlobo <lloyd@lloydlobo.com>
2022-07-07 13:04:28 +0530

    feat: parse data to json clientside browse cheerio

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,35 @@
 "use strict";
-console.log('Gloria In Excelsis Deo!');
 var PORT = 8000;
-console.log(PORT);
-var express = require('express');
-var axios = require('axios');
-var cheerio = require('cheerio');
+var express = require("express");
+var axios = require("axios");
+var cheerio = require("cheerio");
 var app = express();
 var newspapers = [{}];
 var articles = [];
-app.get('/', function (req, res) {
-    res.json('Welcome to favorite lyrics API');
+app.get("/", function (req, res) {
+    res.json("Welcome to favorite lyrics API");
 });
-app.get('/lyrics', function (req, res) {
-    var url = "https://www.billboard.com/music/rock/bob-dylan-beautiful-lyrics-nobel-prize-literature-7541798/";
-    axios.get(url).then(function (response) {
+app.get("/lyrics", function (req, res) {
+    var urls = [
+        "https://www.billboard.com/music/rock/bob-dylan-beautiful-lyrics-nobel-prize-literature-7541798/",
+        "https://www.azlyrics.com/d/dylan.html",
+        "https://www.theguardian.com/uk/money",
+    ];
+    axios.get(urls[1]).then(function (response) {
         var html = response.data;
         console.log(html);
+        var $ = cheerio.load(html);
+        $('a:contains("i")', html).each(function () {
+            var title = $(this).text();
+            var url = $(this).attr("href");
+            articles.push({
+                title: title,
+                url: url,
+            });
+        });
     });
     res.json(articles);
+    console.log(articles);
 });
 app.listen(PORT, function () {
     console.log("server is running at http://localhost:".concat(PORT));
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,61 @@
-console.log('Gloria In Excelsis Deo!');
 // https://www.billboard.com/music/rock/bob-dylan-beautiful-lyrics-nobel-prize-literature-7541798/
 
 const PORT = 8000;
-console.log(PORT);
 
 // initialize
-const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 const app = express();
 
 const newspapers = [{}]; // maybe fetch the newspaper json from live-api rapid-api?
 
 type Article = {
-    title: string;
-    url: string;
-    source: string;
+  title: string;
+  url: string;
+  source?: string;
 };
 const articles: Article[] = [];
 
 // #1 path
-app.get('/', (req: any, res: { json: (arg0: string) => void }) => {
-    res.json('Welcome to favorite lyrics API');
+app.get("/", (req: any, res: any) => {
+  res.json("Welcome to favorite lyrics API");
 });
 
 // #3 fetch lyrics from axios -> html scrape with cheerio
 // and push it to the array articles
-app.get('/lyrics', (req: any, res: any) => {
-    const url = `https://www.billboard.com/music/rock/bob-dylan-beautiful-lyrics-nobel-prize-literature-7541798/`;
+app.get("/lyrics", (req: any, res: any) => {
+  const urls = [
+    "https://www.billboard.com/music/rock/bob-dylan-beautiful-lyrics-nobel-prize-literature-7541798/",
+    "https://www.azlyrics.com/d/dylan.html",
+    "https://www.theguardian.com/uk/money",
+  ];
 
-    // eslint-disable-next-line no-use-before-define
-    axios.get(url).then(function (response: { data: any }) {
-        const html = response.data;
-        console.log(html); // // go visit http://localhost:8000/lyrics -> html of the url websites comes back to the terminal
+  // eslint-disable-next-line no-use-before-define
+  axios.get(urls[1]).then((response: { data: any }) => {
+    const html = response.data;
+    console.log(html); // // go visit http://localhost:8000/lyrics -> html in the terminal
+
+    const $ = cheerio.load(html);
+    // eslint-disable-next-line no-undef
+    $('a:contains("i")', html).each(function (this: cheerio.Element) {
+      const title: string = $(this).text();
+      const url: string = $(this).attr("href");
+
+      articles.push({
+        title: title,
+        url: url,
+      });
     });
-    // res JSON displayed in localhost:${PORT}/lyrics
-    res.json(articles);
+  });
+
+  // res JSON displayed in localhost:${PORT}/lyrics
+  res.json(articles);
+  console.log(articles);
 });
 
 // #2 listen through nodemon - dist/index.js
 app.listen(PORT, () => {
-    console.log(`server is running at http://localhost:${PORT}`);
+  console.log(`server is running at http://localhost:${PORT}`);
 });
