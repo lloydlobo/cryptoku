f47617e138e05f2c8f63afacb33a0e839cbf8c02
lloydlobo <lloyd@lloydlobo.com>
2022-07-07 13:39:19 +0530

    feat: web scrape & fs write output to output.json

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,22 @@ var PORT = 8000;
 var express = require("express");
 var axios = require("axios");
 var cheerio = require("cheerio");
+var fs = require("fs");
+var path = require("path");
 var app = express();
-var newspapers = [{}];
-var articles = [];
+var artists = [{}];
+var songs = [];
+function writeJSONToFile() {
+    var jsonContent = JSON.stringify(songs);
+    fs.writeFile("output.json", jsonContent, "utf8", function (err) {
+        if (err) {
+            console.error(err);
+            throw new Error(err);
+        }
+        var time = new Date(new Date().getTime());
+        console.log("\n".concat(time, "\nFile written to output.json\n\n"));
+    });
+}
 app.get("/", function (req, res) {
     res.json("Welcome to favorite lyrics API");
 });
@@ -15,21 +28,22 @@ app.get("/lyrics", function (req, res) {
         "https://www.azlyrics.com/d/dylan.html",
         "https://www.theguardian.com/uk/money",
     ];
-    axios.get(urls[1]).then(function (response) {
+    axios.get(urls[0]).then(function (response) {
         var html = response.data;
         console.log(html);
         var $ = cheerio.load(html);
-        $('a:contains("i")', html).each(function () {
+        $('strong:contains("I")', html).each(function () {
             var title = $(this).text();
             var url = $(this).attr("href");
-            articles.push({
+            var body = $(this).attr("p");
+            songs.push({
                 title: title,
                 url: url,
             });
         });
+        writeJSONToFile();
     });
-    res.json(articles);
-    console.log(articles);
+    res.json(songs);
 });
 app.listen(PORT, function () {
     console.log("server is running at http://localhost:".concat(PORT));
--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,32 @@ const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
 
-const newspapers = [{}]; // maybe fetch the newspaper json from live-api rapid-api?
+const artists = [{}]; // maybe fetch the newspaper json from live-api rapid-api?
 
-type Article = {
+type Song = {
   title: string;
   url: string;
   source?: string;
+  body?: string;
 };
-const articles: Article[] = [];
+const songs: Song[] = [];
+function writeJSONToFile() {
+  const jsonContent = JSON.stringify(songs);
+
+  fs.writeFile("output.json", jsonContent, "utf8", (err: any) => {
+    if (err) {
+      console.error(err);
+      throw new Error(err);
+    }
+    const time = new Date(new Date().getTime());
+    console.log(`\n${time}\nFile written to output.json\n\n`);
+  });
+}
 
 // #1 path
 app.get("/", (req: any, res: any) => {
@@ -33,29 +49,40 @@ app.get("/lyrics", (req: any, res: any) => {
   ];
 
   // eslint-disable-next-line no-use-before-define
-  axios.get(urls[1]).then((response: { data: any }) => {
+  axios.get(urls[0]).then((response: { data: any }) => {
     const html = response.data;
     console.log(html); // // go visit http://localhost:8000/lyrics -> html in the terminal
 
     const $ = cheerio.load(html);
+    // can use regexp to input RegExp(/[a-zA-Z0-9]) in a:contains()
     // eslint-disable-next-line no-undef
-    $('a:contains("i")', html).each(function (this: cheerio.Element) {
+    $('strong:contains("I")', html).each(function (this: cheerio.Element) {
       const title: string = $(this).text();
       const url: string = $(this).attr("href");
+      const body: string = $(this).attr("p");
 
-      articles.push({
+      songs.push({
         title: title,
         url: url,
       });
     });
+    writeJSONToFile();
   });
 
   // res JSON displayed in localhost:${PORT}/lyrics
-  res.json(articles);
-  console.log(articles);
+  res.json(songs);
 });
 
 // #2 listen through nodemon - dist/index.js
 app.listen(PORT, () => {
   console.log(`server is running at http://localhost:${PORT}`);
 });
+
+// parse json
+// const jsonObj = JSON.parse([...songs].toString());
+
+// stringify JSON object just parsed
+
+// const writeTo = path.join(__dirname, "songs.json");
+// https://www.tutorialkart.com/nodejs/node-js-write-json-object-to-file/
+// const writeSync = fs.writeSync(writeTo);
